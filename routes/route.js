8c56ec226e48a01cae5443171bbb091242fd6fa0
AvutE <tuva@etavo.com>
2018-11-14 11:16:12 +0100

    second try, connected to homeController

---
+++ routes/route.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+var home_controller = require('../controller/homeController');
+
+router.get('/', home_controller.get);
+
+module.export = router;
