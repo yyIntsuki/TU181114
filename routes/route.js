0706441f3c80cdda03c50f53443be33078ef87d7
AvutE <tuva@etavo.com>
2018-11-14 11:06:06 +0100

    connection to homeController

--- routes/route.js
+++ routes/route.js
@@ -1,4 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var home_controller = require('../controller/home.controller');
 
+var home_controller = require('../controller/homeController');
+
+router.get('/', home_controller.get);
+
+module.exports = router;
