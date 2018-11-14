9f6a283be6afa955511299fb2200419eaae224b6
yyIntsuki <intsuki.yz@gmail.com>
2018-11-14 10:57:40 +0100

    Fixed some code in app.js.

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 4242;
 
-app.listen(PORT, function(){
-    console.log('Server up and running at port 4242');
-});
-
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/www/index.html');
+});
+
+app.listen(PORT, function(){
+    console.log('Server up and running at port 4242');
 });
