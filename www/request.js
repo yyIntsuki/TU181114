8feb04281a6dea9ad9f82455c4f3899ad9721765
yyIntsuki <intsuki.yz@gmail.com>
2018-11-16 12:19:07 +0100

    Added Javascript functions for dropdown list.

--- www/request.js
+++ www/request.js
@@ -1,27 +1,50 @@
-function LoadDoc() {
+function loadList() {
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+
+    xhttp.onreadystatechange = 
+    function() {
         if (this.readyState == 4 && this.status == 200) {
-            viewProducts(this);
+            var listItem = JSON.parse(this.responseText);
+            GetList(listItem);
+       }
+    };
+
+    xhttp.open('GET', 'https://hidden-forest-30275.herokuapp.com/products', true);
+    xhttp.send();
+
+    function GetList(item) {
+        var print = '';
+        var i;
+        for (i = 0; i < item.length; i++) {
+            print += '<option value"' + item[i]._id + '">' + item[i].name + '</option>';
+        }
+
+        document.getElementById('dd-list').innerHTML += print;
+    }
+};
+loadList();
+
+function showProduct(id) {
+    var xhttp;
+    if (id == '') {
+        document.getElementById('productInfo').innerHTML = '';
+        return;
+    }
+
+    xhttp = new XMLHttpRequest();
+    
+    xhttp.onreadystatechange = 
+    function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var prodInfo = JSON.parse(this.responseText);
+            document.getElementById('productInfo').innerHTML =
+            '<table>' + 
+            '<tr><th>Product</th><td>' + prodInfo.name + '</td></tr>' +
+            '<tr><th>Price</th><td>' + prodInfo.price + '</td></tr>' +
+            '</table>';
         }
     };
     
-    xhttp.open('GET', 'https://hidden-forest-30275.herokuapp.com/products', true);
+    xhttp.open('GET', 'https://hidden-forest-30275.herokuapp.com/products' + id, true);
     xhttp.send();
-}
-
-function viewProducts(xml) {
-    var i;
-    var xmlDoc = xml.responseXML;
-    var table = '<tr><th>Product</th><th>Price</th></tr>';
-    var x = xmlDoc.getElementsByTagName('products');
-
-    for(i = 0; i < x.length; i++) {
-        table += '<tr><td>' + 
-        x[i].getElementsByTagName('name')[0].childNodes[0].nodeValue +
-        x[i].getElementsByTagName('price')[0].childNodes[0].nodeValue +
-        '</td></tr>';
-    }
-    
-    document.getElementById('productTable').innerHTML = table;
 }
