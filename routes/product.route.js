d7a972e253b181f3b820b5ed09a9ff88c534d1a6
AvutE <tuva@etavo.com>
2018-11-16 12:34:27 +0100

    small change

--- routes/product.route.js
+++ routes/product.route.js
@@ -4,13 +4,9 @@ var router = express.Router();
 var product_controller = require('../controller/product.controller');
 
 router.get('/', product_controller.list);
-
 router.post('/create', product_controller.create);
-
 router.get('/:id', product_controller.details);
-
 router.put('/:id/update', product_controller.update);
-
 router.delete('/:id/delete', product_controller.delete);
 
 module.exports = router;
